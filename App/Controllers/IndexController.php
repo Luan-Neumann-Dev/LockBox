a5d4671339c68e7336a8dca8409c2b062514963e
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-23 19:45:50 -0300

    refactor: routes

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController {
+    public function __invoke() {
+        echo "indexcontroller -- invoke";
+    }
+}
---
+++ App/Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class LoginController {
+
+    public function index() {
+        echo 'Login Controller Index';
+    }
+
+    public function login() {
+        echo 'Login Controller Login';
+    }
+
+}
