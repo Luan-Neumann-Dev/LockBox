6811f4bbae6aaa787d596c84803974ba299e9b55
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-23 19:33:33 -0300

    feat: base project done

---
+++ App/Models/Usuario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class Usuario {
+    public $id;
+    public $name;
+    public $email;
+    public $senha;
+}
---
+++ views/404.view.php
@@ -0,0 +1,3 @@
+<h1 class="text-lg fonr-bold mt-2">
+    Página não encontrada... Perdão
+</h1>
