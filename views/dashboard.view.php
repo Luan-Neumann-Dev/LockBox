c97f4ea73cc5dba2cfc6143ed013c072825dc133
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-23 20:46:32 -0300

    feat: create search layout and menu/form

--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -17,4 +17,35 @@
             </ul>
         </div>
     </div>
+
+    <div class="flex space-x-4 w-full">
+        <form class="w-full">
+            <label class="input input-bordered flex items-center gap-2 w-full">
+                <input type="text" class="grow" name="pesquisar" placeholder="Pesquisar notas no LockBox..."/>
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g
+                            stroke-linejoin="round"
+                            stroke-linecap="round"
+                            stroke-width="2.5"
+                            fill="none"
+                            stroke="currentColor">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+            </label>
+        </form>
+
+        <a href="#" class="btn btn-primary">+ item</a>
+    </div>
+
+    <div class="h-svh flex py-6">
+        <div class="bg-base-300 rounded-l-box w-56">
+
+        </div>
+
+        <div class="bg-base-200 rounded-r-box w-full p-10">
+            form
+        </div>
+    </div>
 </div>
